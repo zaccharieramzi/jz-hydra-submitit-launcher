5652b2a93d5934c74056228ac883fc35328b2d5c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2022-01-07 16:21:32 +0100

    put the package info in init rather than setup

--- setup.py
+++ setup.py
@@ -7,16 +7,21 @@ with open("README.md", "r") as fh:
 with open('requirements.txt') as open_file:
     install_requires = open_file.read()
 
+import jz_hydra_submitit_launcher
+
 setuptools.setup(
     name="jz-hydra-submitit-launcher",
-    version="0.0.1",
-    author="Zaccharie Ramzi",
-    author_email="zaccharie.ramzi@gmail.com",
-    description="Jean Zay tailored Hydra submitit launcher.",
+    version=jz_hydra_submitit_launcher.__version__,
+    author=jz_hydra_submitit_launcher.__author__,
+    author_email=jz_hydra_submitit_launcher.__author_email__,
+    description=jz_hydra_submitit_launcher.__docs__,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    homepage=jz_hydra_submitit_launcher.__homepage__,
     url="https://github.com/zaccharieramzi/jz-hydra-submitit-launcher",
+    license=jz_hydra_submitit_launcher.__license__,
     packages=setuptools.find_packages(),
+    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -25,4 +30,5 @@ setuptools.setup(
     install_requires=install_requires,
     python_requires='>=3.6',
     include_package_data=True,
+    keywords=['hydra', 'submitit', 'jean-zay'],
 )
