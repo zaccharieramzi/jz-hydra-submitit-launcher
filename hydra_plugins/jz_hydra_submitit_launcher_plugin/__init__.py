645e36e6d05d62ce5690f87efd4cd8895ba2659b
Jasha <8935917+Jasha10@users.noreply.github.com>
2022-01-18 15:31:37 -0600

    use `hydra_plugins` namespace package

---
+++ hydra_plugins/jz_hydra_submitit_launcher_plugin/__init__.py
@@ -0,0 +1,8 @@
+"""Package info"""
+
+__version__ = "develop"
+__author__ = 'Zaccharie Ramzi'
+__author_email__ = 'zaccharie.ramzi@gmail.com'
+__license__ = 'MIT'
+__homepage__ = 'https://github.com/zaccharieramzi/jz-hydra-submitit-launcher'
+__docs__ = 'Jean Zay tailored Hydra submitit launcher.'
--- hydra_plugins/jz_hydra_submitit_launcher_plugin/searchpath_config.py
+++ hydra_plugins/jz_hydra_submitit_launcher_plugin/searchpath_config.py
@@ -2,7 +2,6 @@
 from hydra.core.config_search_path import ConfigSearchPath
 from hydra.plugins.search_path_plugin import SearchPathPlugin
 
-
 class JeanZaySearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
         # Appends the search path for this plugin to the end of the search path
