eef7bec5179dfa22383b54890fb9f6910af59102
zaccharieramzi <email@example.com>
2022-01-20 13:57:29 +0100

    added a new qos resolver

--- hydra_plugins/jz_hydra_submitit_launcher/resolvers.py
+++ hydra_plugins/jz_hydra_submitit_launcher/resolvers.py
@@ -1,3 +1,15 @@
 from omegaconf import OmegaConf
 
-OmegaConf.register_new_resolver("multiply10", lambda x: x * 10)
+
+def time_to_qos(timeout_hour):
+    if timeout_hour > 20:
+        qos = 't4'
+    elif timeout_hour > 2:
+        qos = 't3'
+    else:
+        qos = 'dev'
+
+    qos = f'qos_gpu-{qos}'
+    return qos
+
+OmegaConf.register_new_resolver("qos_from_hours", time_to_qos)
