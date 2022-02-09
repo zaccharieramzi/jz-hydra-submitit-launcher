a944ea563c42cb09b9b4032f4ba8116cf4debf96
zaccharieramzi <email@example.com>
2022-02-09 12:09:46 +0100

    TMP added cpu count logging in example app

--- example_app.py
+++ example_app.py
@@ -2,10 +2,13 @@ import hydra
 
 from omegaconf import OmegaConf
 
+import multiprocessing
+
 
 @hydra.main(config_path='conf', config_name='config')
 def example_main(cfg):
     print(OmegaConf.to_container(cfg, resolve=True))
+    print(multiprocessing.cpu_count())
 
 if __name__ == '__main__':
     example_main()
